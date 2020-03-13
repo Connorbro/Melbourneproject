0e70607c64388c05e2b874bf48d5f6cff018209f
ericdiedrich <ericdiedrich@users.noreply.github.com>
2020-03-13 16:45:08 +0000

    fixed-css

--- src/components/About.js
+++ src/components/About.js
@@ -6,33 +6,25 @@ import twitter from './twitter.png'
 
 export default function Navbar() {
     return(
-  <div>
-      <h1>About Us</h1>
+      <div>
+        <h1 className="title-h1">About Us</h1>
 
-      <p>We are the number one tourist website around Australia! We find you the greatest price for tours in Melbourne. </p>
+        <p>We are the number one tourist website around Australia! We find you the greatest price for tours in Melbourne. </p>
 
-      <p>All of our unique tours are different from different levels of absailing if you are a thril seaker,
-       or if you just want something calm. There are plenty of different options to do such as bus tours, or a morning walk tour.</p>
+        <p>All of our unique tours are different from different levels of absailing if you are a thril seaker,
+        or if you just want something calm. There are plenty of different options to do such as bus tours, or a morning walk tour.</p>
 
-       <p>So as you can see we provide something for everyone, whether you are 16 or 75 we have it all! </p>
+        <p>So as you can see we provide something for everyone, whether you are 16 or 75 we have it all! </p>
 
-       <h2>Contact us</h2>
+        <h2>Contact us</h2>
 
 
-       <div class="row">
-  <div class="column">
-    <img src={facebook} alt=""/>
-  </div>
-  <div class="column">
-    <img src={twitter} alt="" />
-  </div>
-  <div class="column">
-    <img src={linkedin} alt="" />
-  </div>
-</div>
-
-     
-  </div>
+        <div className="social-div">
+          <img className="social-img" src={facebook} alt=""/>
+          <img className="social-img" src={twitter} alt="" />
+          <img className="social-img" src={linkedin} alt="" />
+        </div>
+      </div>
     );
 }
 
--- src/components/Pricefilter.js
+++ src/components/Pricefilter.js
@@ -15,28 +15,21 @@ function Pricefilter(props) {
 
     
     return(
-        <div className="wholecard">
+        <div className="">
             {
                 props.details.data.results && filteredPrices.map( (obj, index) => {
                     return (
-                        <div key={index}>
-                            <div className="names">
-                                <h1>
-                                <br/>
-                                {obj.name}</h1></div>
-                                <div className="price">
-                                <h2>Price: ${obj.price.amount} </h2></div>
-                                <br/>
-                                <div className="url">
-                                <br/>
+                        <div className="wholecard" key={index}>
+                            <div>
+                                <h1>{obj.name}</h1>
+                                <h2>Price: ${obj.price.amount} </h2>
                                 <a href={obj.vendor_tour_url}>Click here to book now!</a>
-                                </div>
                                 <div className="images">
-                                <br/>
                                 
-                                <img src={obj.images[0].source_url} alt=""/>
+                                    <img className="apiimg" src={obj.images[0].source_url} alt=""/>
                                 </div>
                             </div>
+                        </div>
                       )
                     })
                 }           
